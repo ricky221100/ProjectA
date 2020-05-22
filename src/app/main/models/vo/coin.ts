f96b5f58705f271d0ca92c12155ea424f51cacb2
ricky221100 <riccardo.lattuada99@outlook.it>
2020-05-22 15:29:35 +0200

    start

---
+++ src/app/main/models/vo/coin.ts
@@ -0,0 +1,11 @@
+export class Coin {
+    public id: string = undefined;
+    public value:string = undefined;
+    public name:string = undefined;
+    public description:string = undefined;
+    /**
+     * metodo statico utilizzato per recuperare l'id dell'entita.
+     * @param item
+     */
+    static selectId: (item: Coin) => string = item => item.id;
+}
